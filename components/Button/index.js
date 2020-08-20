4674ebc60e3ec39fd2aa3ca4fa74a8a3eb5d02e4
por <pdumrhi@sertiscorp.com>
2020-08-20 16:49:51 +0700

    v0.1.1b

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-const Button = ({children}) => (
-  <button>{children}!!</button>
-)
+
+function Button(props) {
+  const { children } = props
+  return <button>{children}!!</button>
+}
+
+
+// const Button = ({children}) => (
+//   <button>{children}!!</button>
+// )
 
 export default Button
---
+++ stories/Button.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Button from '../components/Button'
+export default { title: 'Button', component: Button }
+
+const Template = () => <Button>test</Button>
+
+export const FirstStory = Template.bind({})
+Template.args = {};
